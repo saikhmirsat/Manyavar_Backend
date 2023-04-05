4d5130f1090d1f334bd39b71a6857570d597f912
Prakashkumarjena700 <prakashkumarjena367@gmail.com>
2023-04-05 12:20:51 +0530

    Added update and delete in product route

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -9,7 +9,7 @@ productRoute.get("/", async (req, res) => {
         const Products = await productModel.find()
         res.send(Products)
     } catch (err) {
-        res.send('Can not found')
+        res.send({ "msg": "Can not found", "sucess": false })
         console.log(err)
     }
 })
@@ -18,7 +18,7 @@ productRoute.get("/:_id", async (req, res) => {
         const Product = await productModel.find({ _id: req.params })
         res.send(Product)
     } catch (err) {
-        res.send('Can not found')
+        res.send({ "msg": "Can not found", "sucess": false })
         console.log(err)
     }
 })
@@ -27,9 +27,32 @@ productRoute.post("/add", async (req, res) => {
     try {
         const data = new productModel(req.body)
         await data.save()
-        res.send({ msg: 'Product has been added successfully' })
+        res.send({ msg: "Product has been added successfully", "sucess": true })
     } catch (err) {
-        res.send({ msg: 'Not added' })
+        res.send({ msg: "Not added", "sucess": false })
+        console.log(err)
+    }
+})
+
+productRoute.patch("/update/:_id", async (req, res) => {
+    try {
+        const _id = req.params._id
+        const payload = req.body
+        await productModel.findByIdAndUpdate({ _id }, payload)
+        res.send({ msg: "Product has been updated successfully", "sucess": true })
+    } catch (err) {
+        res.send({ msg: "Not updated", "sucess": false })
+        console.log(err)
+    }
+})
+
+productRoute.delete("/delete/:_id", async (req, res) => {
+    try {
+        const _id = req.params._id
+        await productModel.findByIdAndDelete({ _id })
+        res.send({ msg: "Product has been deleted successfully", "sucess": true })
+    } catch (err) {
+        res.send({ msg: "Not deleted", "sucess": false })
         console.log(err)
     }
 })
